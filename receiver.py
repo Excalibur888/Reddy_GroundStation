1e04218d9159ebc32ec01c5e00220421b12688fb
Excalibur888 <arthurdumond19100@gmail.com>
2024-05-07 23:23:53 +0900

    Settings updated

--- receiver.py
+++ receiver.py
@@ -74,7 +74,7 @@ while True :
 
         # Print packet/signal status including RSSI, SNR, and signalRSSI
         print("Packet status: RSSI = {0:0.2f} dBm | SNR = {1:0.2f} dB".format(LoRa.packetRssi(), LoRa.snr()))
-
+        print("")
         # Show received status in case CRC or header error occur
         status = LoRa.status()
         if status == LoRa.STATUS_CRC_ERR : print("CRC error")
